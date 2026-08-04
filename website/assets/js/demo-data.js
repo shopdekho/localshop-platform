b9fda83ba87c9baf41fc2b7743fa2fc115982b79
shopdekho <163655602+shopdekho@users.noreply.github.com>
2026-08-04 13:56:24 +0530

    Create demo-data.js

---
+++ website/assets/js/demo-data.js
@@ -0,0 +1,21 @@
+const shop={
+
+id:"PRY8A4C2",
+
+name:{
+
+en:"Sharma Vegetables",
+
+hi:"शर्मा सब्जी भंडार"
+
+},
+
+banner:"assets/images/demo/banner.webp",
+
+logo:"assets/images/demo/shop.webp",
+
+rating:"4.8",
+
+address:"Near SBI Bank, Prayagraj"
+
+};
